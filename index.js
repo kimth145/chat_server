4cb0547355bc90e27efc0d07579383e00b29eeb7
kimth <kimth145@ais-info.co.jp>
2024-01-18 16:30:55 +0900

    chat_server

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, {cors:{origin:"*"}})
+
+let userInfo = []
+
+io.on('connection', (socket)=>{
+    console.log("user connected!!")
+
+    socket.on('disconnect', ()=>{
+        const user = userInfo.find(c=>c.id == socket.id)
+        console.log(user)
+
+        socket.broadcast.emit('msg',{level:"sys" ,msg:user.nickName+"님이 퇴장하였습니다"})
+    })
+
+    socket.on('login', (nickName)=>{
+        const info = {
+            nickName:nickName,
+            id:socket.id
+        }
+        userInfo.push(info)
+        io.emit('msg',{level:"sys" ,msg:nickName+"님이 입장하였습니다"})
+    })
+
+    socket.on('send', (msg1)=>{
+        socket.broadcast.emit('msg',{level:"" ,msg:msg1})
+    })
+
+})
+
+http.listen(process.env.PORT || 5000, ()=>{
+    console.log("connected!!!")
+})
